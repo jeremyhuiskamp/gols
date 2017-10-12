2309ca29346a918b88ba8a2b0e1c39c37c91424b
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2017-10-12 23:01:03 +0200

    initial implementation
    The parser is adapted from http://norvig.com/lispy.html
    
    The rest is a rough translation from The Little Schemer, chapter 10 into
    go, with a bit of additional type and error checking.  It's bad go, but
    it works.

---
+++ gols.go
@@ -0,0 +1,395 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+// entry is one layer in a symbol table.
+// Don't put lists in here -> runtime panic.
+// Are nums allowed?  Can we limit the keys to strings?
+type entry map[interface{}]interface{}
+
+// lookup finds the value of a name in an entry.
+func (e entry) lookup(name interface{}) (interface{}, bool) {
+	if res, ok := e[name]; ok {
+		return res, true
+	}
+	return nil, false
+}
+
+// table is a symbol table.
+type table []entry
+
+// lookup finds the value of a name in a table.
+func (t table) lookup(name interface{}) (interface{}, bool) {
+	for _, e := range t {
+		if val, ok := e.lookup(name); ok {
+			return val, true
+		}
+	}
+	return nil, false
+}
+
+type action func(interface{}, table) (interface{}, error)
+
+func constAction(sexp interface{}, t table) (interface{}, error) {
+	if num, ok := sexp.(uint64); ok {
+		return num, nil
+	}
+	if sexp == "#t" || sexp == "#f" {
+		return sexp, nil
+	}
+	return []interface{}{"primitive", sexp}, nil
+}
+
+func quoteAction(sexp interface{}, t table) (interface{}, error) {
+	if list, ok := sexp.([]interface{}); !ok {
+		return nil, errors.New("quote requires a list")
+	} else if len(list) != 2 {
+		return nil, errors.New("quote must be a list with two elements")
+	} else {
+		return list[1], nil
+	}
+}
+
+func identifierAction(sexp interface{}, t table) (interface{}, error) {
+	if name, ok := sexp.(string); !ok {
+		// is this a bug in the interpreter?
+		return nil, errors.New("identifiers must be atoms")
+	} else if val, ok := t.lookup(name); !ok {
+		return nil, fmt.Errorf("unrecognized identifier: %q", name)
+	} else {
+		return val, nil
+	}
+}
+
+func lambdaAction(sexp interface{}, t table) (interface{}, error) {
+	lambda, ok := sexp.([]interface{})
+	if !ok {
+		return nil, errors.New("lambda requires a list")
+	} else if len(lambda) != 3 {
+		return nil, errors.New("lambda requires a list with three elements")
+	}
+	// further verification left to the application:
+	return []interface{}{
+		"non-primitive",
+		[]interface{}{
+			t,         // hmm, t isn't an s-exp...
+			lambda[1], // formals
+			lambda[2], // body expression
+		},
+	}, nil
+}
+
+func condAction(sexp interface{}, t table) (interface{}, error) {
+	cond, ok := sexp.([]interface{})
+	if !ok {
+		return nil, errors.New("cond requires a list")
+	}
+	lines := cond[1:] // skip "cond" keyword
+	for _, line := range lines {
+		if cline, ok := line.([]interface{}); !ok {
+			return nil, errors.New("cond lines must be lists")
+		} else if len(cline) != 2 {
+			return nil, errors.New("cond lines must be lists with two elements")
+		} else if cline[0] == "else" {
+			return meaning(cline[1], t)
+		} else {
+			matches, err := meaning(cline[0], t)
+			if err != nil {
+				return nil, err
+			}
+			// Only place where booleans are significant in
+			// the language?
+			// Is it an error if the meaning isn't boolean?
+			if matches == "#t" {
+				return meaning(cline[1], t)
+			}
+		}
+		// do we want to validate the syntax of what comes after
+		// a match?  eg, missing else, stuff after an else, etc
+	}
+	return nil, errors.New("cond must have an else line")
+}
+
+func applicationAction(sexp interface{}, t table) (interface{}, error) {
+	list, ok := sexp.([]interface{})
+	if !ok {
+		return nil, errors.New("application requires a list")
+	}
+	if len(list) == 0 {
+		return nil, errors.New("application requires a non-empty list")
+	}
+
+	fMeaning, err := meaning(list[0], t)
+	if err != nil {
+		return nil, err
+	}
+	// either (primitive foo) or (non-primitive (table formals body))
+	f, ok := fMeaning.([]interface{})
+	// I think these are bugs in the interpreter?
+	if !ok {
+		return nil, errors.New("the meaning of a function application must be a list")
+	}
+	if len(f) != 2 {
+		return nil, errors.New(
+			"the meaning of a function application must be a " +
+				"list with two elements")
+	}
+
+	args := list[1:]
+
+	argVals := []interface{}{}
+	for _, arg := range args {
+		argVal, err := meaning(arg, t)
+		if err != nil {
+			return nil, err
+		}
+		argVals = append(argVals, argVal)
+	}
+
+	if f[0] == "primitive" {
+		if name, ok := f[1].(string); !ok {
+			return nil, errors.New("name of primitive function must be a string")
+		} else {
+			return applyPrimitive(name, argVals)
+		}
+	} else if f[0] == "non-primitive" {
+		// f[1] is (table formals body)
+		p, ok := f[1].([]interface{})
+		if !ok || len(p) != 3 {
+			// bug in lambdaAction...
+			return nil, errors.New("non-primitive should have three args")
+		}
+		// how is this different than the table passed to this function?
+		t, ok := p[0].(table)
+		if !ok {
+			return nil, errors.New("non-primitive needs a table")
+		}
+		formals, ok := p[1].([]interface{})
+		if !ok {
+			return nil, errors.New("non-primitive requires formals")
+		}
+		if len(formals) != len(argVals) {
+			return nil, errors.New("mismatching number of arguments and parameters")
+		}
+		e := entry(map[interface{}]interface{}{})
+		for i, _ := range formals {
+			e[formals[i]] = argVals[i]
+		}
+		t = append(table([]entry{e}), t...)
+		return meaning(p[2], t)
+	} else {
+		return nil, fmt.Errorf("unsupported application type: %q", f[0])
+	}
+}
+
+func meaning(sexp interface{}, t table) (interface{}, error) {
+	action := expressionToAction(sexp)
+	return action(sexp, t)
+}
+
+func value(sexp interface{}) (interface{}, error) {
+	return meaning(sexp, table([]entry{}))
+}
+
+func expressionToAction(sexp interface{}) action {
+	if list, ok := sexp.([]interface{}); ok {
+		if len(list) > 0 {
+			if first, ok := list[0].(string); ok {
+				switch first {
+				case "quote":
+					return quoteAction
+				case "lambda":
+					return lambdaAction
+				case "cond":
+					return condAction
+				}
+			}
+		}
+		// applicationAction is going to have to do quite a
+		// lot of error handling!
+		return applicationAction
+	} else {
+		if _, ok := sexp.(uint64); ok {
+			return constAction
+		}
+		switch sexp {
+		case "#t", "#f",
+			"cons", "car", "cdr",
+			"null?", "eq?", "atom?",
+			"zero?", "add1", "sub1",
+			"number?":
+			return constAction
+		default:
+			return identifierAction
+		}
+	}
+}
+
+// applyPrimitive applies a primitive function.
+func applyPrimitive(name string, vals []interface{}) (interface{}, error) {
+	bToSexp := func(b bool) interface{} {
+		if b {
+			return "#t"
+		}
+		return "#f"
+	}
+
+	switch name {
+	case "cons":
+		if len(vals) != 2 {
+			return nil, errors.New("cons takes two arguments")
+		} else if to, ok := vals[1].([]interface{}); !ok {
+			return nil, errors.New("second argument to cons must be a list")
+		} else {
+			return append([]interface{}{vals[0]}, to...), nil
+		}
+	case "car":
+		if len(vals) != 1 {
+			return nil, errors.New("car takes one argument")
+		} else if from, ok := vals[0].([]interface{}); !ok {
+			return nil, errors.New("car takes one list")
+		} else if len(from) < 1 {
+			return nil, errors.New("cannot take car of empty list")
+		} else {
+			return from[0], nil
+		}
+	case "cdr":
+		if len(vals) != 1 {
+			return nil, errors.New("cdr takes one argument")
+		} else if from, ok := vals[0].([]interface{}); !ok {
+			return nil, errors.New("cdr takes one list")
+		} else if len(from) < 1 {
+			return nil, errors.New("cannot take cdr of empty list")
+		} else {
+			return from[1:], nil
+		}
+	case "null?":
+		if len(vals) != 1 {
+			return nil, errors.New("null? takes one argument")
+		} else if from, ok := vals[0].([]interface{}); !ok {
+			return nil, errors.New("null? takes one list")
+		} else {
+			return bToSexp(len(from) == 0), nil
+		}
+	case "eq?":
+		if len(vals) != 2 {
+			return nil, errors.New("eq? takes two arguments")
+		} else if first, ok := vals[0].(string); !ok {
+			return nil, errors.New("eq? takes two atoms")
+		} else if second, ok := vals[1].(string); !ok {
+			return nil, errors.New("eq? takes two atoms")
+		} else {
+			return bToSexp(first == second), nil
+		}
+	case "atom?":
+		if len(vals) != 1 {
+			return nil, errors.New("atom? takes one argument")
+		}
+		// Hmm, support for (primitive x) and (non-privitive x)?
+		// The book suggests these are atoms.  How do we hit that case?
+		_, ok := vals[0].([]interface{})
+		return bToSexp(!ok), nil
+	case "zero?":
+		if len(vals) != 1 {
+			return nil, errors.New("zero? takes one argument")
+		} else if num, ok := vals[0].(uint64); !ok {
+			return nil, errors.New("zero? takes one number")
+		} else {
+			return bToSexp(num == 0), nil
+		}
+	case "add1":
+		if len(vals) != 1 {
+			return nil, errors.New("add1 takes one argument")
+		} else if num, ok := vals[0].(uint64); !ok {
+			return nil, errors.New("add1 takes one number")
+		} else if num == math.MaxUint64 {
+			return nil, errors.New("add1 would cause overflow")
+		} else {
+			return num + 1, nil
+		}
+	case "sub1":
+		if len(vals) != 1 {
+			return nil, errors.New("sub1 takes one argument")
+		} else if num, ok := vals[0].(uint64); !ok {
+			return nil, errors.New("sub1 takes one number")
+		} else if num == 0 {
+			return nil, errors.New("sub1 would cause underflow")
+		} else {
+			return num - 1, nil
+		}
+	case "number?":
+		if len(vals) != 1 {
+			return nil, errors.New("number? takes one argument")
+		}
+		_, ok := vals[0].(uint64)
+		return bToSexp(ok), nil
+	default:
+		return nil, fmt.Errorf("unknown primitive: %q", name)
+	}
+}
+
+// parsing implementation below copied from http://norvig.com/lispy.html
+
+// tokenize tokenizes an s-expression where only unicode whitespace and
+// ()s are considered significant.
+func tokenize(src string) []string {
+	src = strings.Replace(src, "(", " ( ", -1)
+	src = strings.Replace(src, ")", " ) ", -1)
+	return strings.Fields(src)
+}
+
+// readFromTokens builds an abstract syntax tree from a list of tokens.
+// Atoms are either a string or a uint64.  Lists are a []interface{}.
+// TODO: consider #f and #t as bool types?
+func readFromTokens(tokens []string) (interface{}, []string, error) {
+	if len(tokens) == 0 {
+		return nil, nil, errors.New("unexpected EOF")
+	}
+
+	token := tokens[0]
+	tokens = tokens[1:]
+
+	switch token {
+	case "(":
+		l := []interface{}{} // NB: empty list, not nil
+		for len(tokens) > 0 && tokens[0] != ")" {
+			sexp, remainder, err := readFromTokens(tokens)
+			if err != nil {
+				return nil, nil, err
+			}
+			tokens = remainder
+			l = append(l, sexp)
+		}
+		if len(tokens) < 1 {
+			return nil, nil, errors.New("unfinished list")
+		}
+		return l, tokens[1:], nil
+	case ")":
+		return nil, nil, errors.New("unexpected )")
+	default:
+		if num, err := strconv.ParseUint(token, 10, 64); err != nil {
+			return token, tokens, nil
+		} else {
+			return num, tokens, nil
+		}
+	}
+}
+
+// parse tokenizes and builds a syntax tree from an s-expression.
+func parse(src string) (interface{}, error) {
+	tokens := tokenize(src)
+	ast, remainder, err := readFromTokens(tokens)
+	if err != nil {
+		return nil, err
+	}
+	if len(remainder) > 0 {
+		return nil, errors.New("unexpected trailing tokens")
+	}
+	return ast, nil
+}
---
+++ gols_test.go
@@ -0,0 +1,358 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestMeaningValid(t *testing.T) {
+	for in, out := range map[string]string{
+		"1":             "1",
+		"(quote (foo))": "(foo)",
+		"(quote foo)":   "foo",
+
+		"(atom? 1)":          "#t",
+		"(atom? (quote x))":  "#t",
+		"(atom? (quote ()))": "#f",
+
+		"(null? (quote ()))":  "#t",
+		"(null? (quote (x)))": "#f",
+
+		"(eq? (quote x) (quote x))": "#t",
+		"(eq? (quote x) (quote y))": "#f",
+
+		"(number? 1)":          "#t",
+		"(number? (quote x))":  "#f",
+		"(number? (quote ()))": "#f",
+
+		"(zero? 0)": "#t",
+		"(zero? 1)": "#f",
+
+		"(add1 0)": "1",
+		"(sub1 1)": "0",
+
+		"(cons 1 (quote ()))":            "(1)",
+		"(cons 1 (quote (2)))":           "(1 2)",
+		"(cons (quote (1)) (quote (2)))": "((1) 2)",
+
+		"(car (quote (1 2)))":         "1",
+		"(car (quote ((1) 2)))":       "(1)",
+		"(car (car (quote ((1) 2))))": "1",
+
+		"(cdr (quote (1)))":           "()",
+		"(cdr (quote (1 2)))":         "(2)",
+		"(cdr (quote (1 2 3)))":       "(2 3)",
+		"(cdr (cdr (quote (1 2 3))))": "(3)",
+
+		"(cond (#t 1) (else 2))":             "1",
+		"(cond (#f 1) (else 2))":             "2",
+		"(cond ((number? 1) #t) (else #f))":  "#t",
+		"(cond ((number? #t) #t) (else #f))": "#f",
+
+		"((lambda (f a) (f a)) (lambda (a) (add1 a)) 2)": "3",
+
+		`(((lambda (le)
+	 	    ((lambda (f) (f f))
+		       (lambda (f) (le (lambda (x) ((f f) x))))))
+		   (lambda (length)
+		     (lambda (l)
+		       (cond
+		         ((null? l) 0)
+		         (else (add1 (length (cdr l))))))))
+		  (quote (a b c d)))`: "4",
+	} {
+		t.Log(in)
+		inExp, err := parse(in)
+		if err != nil {
+			t.Fatalf("parse error: %s", err) // test bug
+		}
+
+		outExp, err := value(inExp)
+		if err != nil {
+			t.Fatalf("evaluation error: %s", err) // test bug
+		}
+
+		got := sexpToString(outExp)
+		if got != out {
+			t.Fatalf("unexpected output: %q", got)
+		}
+	}
+}
+
+func TestMeaningInvalid(t *testing.T) {
+	// This is an attempt to hit all hittable error handling.
+	// It's verified mostly by code coverage because it's not
+	// clear exactly how to assert on what error has been
+	// returned.
+	// Not all error handling is hittable, because there's some
+	// safety checks that would represent bugs in the interpreter.
+	for _, in := range []string{
+		"x", // unknown identifier
+
+		"(quote)",     // nothing to quote
+		"(quote a b)", // too many things to quote
+
+		"(lambda)",         // no formals
+		"(lambda (x))",     // no body
+		"(lambda (x) 2 3)", // too much body
+
+		"(cond 1)",       // line not a list
+		"(cond (1))",     // not enough elements in line
+		"(cond (1 2 3))", // too many elements in line
+		"(cond ((x) 2))", // meaning of condition undefined
+		"(cond (#f 2))",  // no match
+
+		"()",                   // application of nothing
+		"(1)",                  // application of a non-function
+		"((quote 1))",          // application of a non-function
+		"(null? x)",            // undefined arguments to function
+		"((lambda 1 2) 3)",     // formals not a list
+		"((lambda (x) 2) 3 4)", // too many arguments
+
+		"(cons 1)",   // need a second argument
+		"(cons 1 2)", // second argument must be a list
+
+		"(car)",               // no arguments
+		"(car (quote (1)) 2)", // too many arguments
+		"(car 1)",             // argument not a list
+		"(car (quote ()))",    // empty list
+
+		"(cdr)",               // no arguments
+		"(cdr (quote (1)) 2)", // too many arguments
+		"(cdr 1)",             // argument not a list
+		"(cdr (quote ()))",    // empty list
+
+		"(null?)",     // no arguments
+		"(null? 1 2)", // too many arguments
+		"(null? 1)",   // argument not a list
+
+		"(eq?)",                       // no arguments
+		"(eq? 1)",                     // not enough arguments
+		"(eq? 1 2 3)",                 // too many arguments
+		"(eq? 1 2)",                   // arguments are numeric
+		"(eq? #f 2)",                  // arguments are numeric
+		"(eq? (quote ()) (quote ()))", // arguments are not atoms
+
+		"(atom?)",     // no arugments
+		"(atom? 1 2)", // too many arguments
+
+		"(zero?)",     // no arguments
+		"(zero? 1 2)", // too many arguments
+		"(zero? #f)",  // argument not a number
+
+		"(add1)",                      // no arguments
+		"(add1 2 3)",                  // too many arguments
+		"(add1 (quote ()))",           // argument not a number
+		"(add1 18446744073709551615)", // overflow
+
+		"(sub1)",     // no arguments
+		"(sub1 2 3)", // too many arguments
+		"(sub1 #f)",  // argument not a number
+		"(sub1 0)",   // underflow
+
+		"(number?)",     // no arguments
+		"(number? 2 3)", // too many arguments
+	} {
+		inExp, err := parse(in)
+		if err != nil {
+			t.Fatalf("parse error: %s", err) // test bug
+		}
+
+		_, err = value(inExp)
+		if err == nil {
+			t.Fatalf("got no error for input %s", in)
+		} else {
+			t.Logf("%s -> %s", in, err)
+		}
+	}
+}
+
+func TestTokenize(t *testing.T) {
+	for _, test := range []struct {
+		src string
+		exp []string
+	}{
+		{
+			"",
+			[]string{},
+		},
+		{
+			"x",
+			[]string{"x"},
+		},
+		{
+			" x\t 1 \n x",
+			[]string{"x", "1", "x"},
+		},
+		{
+			"()",
+			[]string{"(", ")"},
+		},
+		{
+			"(x)",
+			[]string{"(", "x", ")"},
+		},
+	} {
+		name := fmt.Sprintf("test: %q", test.src)
+		t.Run(name, func(t *testing.T) {
+			got := tokenize(test.src)
+			if !reflect.DeepEqual(got, test.exp) {
+				t.Fatalf("unexpected parsed tokens: %#v", got)
+			}
+		})
+	}
+}
+
+func TestReadFromTokens(t *testing.T) {
+	for _, test := range []struct {
+		tokens    []string
+		err       bool
+		tree      interface{}
+		remainder []string
+	}{
+		{
+			tokens: []string{},
+			err:    true,
+		},
+		{
+			tokens: []string{"x"},
+			tree:   "x",
+		},
+		{
+			tokens: []string{"1"},
+			tree:   uint64(1),
+		},
+		{
+			tokens: []string{"(", "x", ")"},
+			tree:   []interface{}{"x"},
+		},
+		{
+			tokens: []string{"(", ")"},
+			tree:   []interface{}{},
+		},
+		{
+			tokens: []string{"(", "(", ")", "x", ")"},
+			tree:   []interface{}{[]interface{}{}, "x"},
+		},
+		{
+			tokens: []string{"(", "x", "1", "*", ")"},
+			tree:   []interface{}{"x", uint64(1), "*"},
+		},
+		{
+			tokens: []string{"(", "x"},
+			err:    true,
+		},
+		{
+			tokens: []string{")", "x"},
+			err:    true,
+		},
+		{
+			tokens: []string{"(", "(", "x"},
+			err:    true,
+		},
+		{
+			tokens:    []string{"(", "x", ")", "x"},
+			tree:      []interface{}{"x"},
+			remainder: []string{"x"},
+		},
+		{
+			tokens:    []string{"x", "x"},
+			tree:      "x",
+			remainder: []string{"x"},
+		},
+	} {
+		name := fmt.Sprintf("test: %q", test.tokens)
+		t.Run(name, func(t *testing.T) {
+			if test.remainder == nil {
+				test.remainder = []string{}
+			}
+			got, remainder, err := readFromTokens(test.tokens)
+			if test.err {
+				if err == nil {
+					t.Fatal("didn't get expected error")
+				}
+			} else if err != nil {
+				t.Fatal(err)
+			} else if !reflect.DeepEqual(got, test.tree) {
+				t.Fatalf("unexpected tree: %#v", got)
+			} else if !reflect.DeepEqual(remainder, test.remainder) {
+				t.Fatalf("unexpected remainder: %#v", remainder)
+			}
+		})
+	}
+}
+
+func TestParse(t *testing.T) {
+	for _, test := range []struct {
+		src  string
+		err  bool
+		tree interface{}
+	}{
+		{
+			src:  "x",
+			tree: "x",
+		},
+		{
+			src: "(x",
+			err: true, // can't readFromTokens
+		},
+		{
+			src: "x x",
+			err: true, // trailing tokens
+		},
+		{
+			src: "(begin (define r 10) (* pi (* r r)))",
+			tree: []interface{}{
+				"begin",
+				[]interface{}{
+					"define",
+					"r",
+					uint64(10),
+				},
+				[]interface{}{
+					"*",
+					"pi",
+					[]interface{}{
+						"*",
+						"r",
+						"r",
+					},
+				},
+			},
+		},
+	} {
+		name := fmt.Sprintf("test: %q", test.src)
+		t.Run(name, func(t *testing.T) {
+			got, err := parse(test.src)
+			if test.err {
+				if err == nil {
+					t.Fatal("didn't get expected error")
+				}
+			} else if err != nil {
+				t.Fatal(err)
+			} else if !reflect.DeepEqual(got, test.tree) {
+				t.Fatalf("unexpected tree: %#v", got)
+			}
+		})
+	}
+}
+
+func sexpToString(sexp interface{}) string {
+	res := ""
+	if l, ok := sexp.([]interface{}); ok {
+		res += "("
+		for i, item := range l {
+			res += sexpToString(item)
+			if i < len(l)-1 {
+				res += " "
+			}
+		}
+		res += ")"
+	} else if i, ok := sexp.(uint64); ok {
+		res += fmt.Sprintf("%d", i)
+	} else {
+		res += fmt.Sprintf("%s", sexp)
+	}
+	return res
+}
